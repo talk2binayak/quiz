f7d4b2dd9a1ece2096a4b1db3525238da799e7e4
Maastrix <maastest3@gmail.com>
2023-08-28 19:36:06 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$jsonString = file_get_contents('data.json');
+
+$data = json_decode($jsonString, true);
+
+if ($data === null) {
+    die('Error parsing JSON.');
+}
+
+$quiz = $data['quiz'];
+
+foreach ($quiz as $category => $questions) {
+    echo "<h2>$category</h2>";
+
+    foreach ($questions as $questionKey => $questionData) {
+        $question = $questionData['question'];
+        $options = $questionData['options'];
+        $answer = $questionData['answer'];
+
+        echo "<p><strong>Question ($questionKey):</strong> $question</p>";
+        
+        echo "<ul>";
+        foreach ($options as $option) {
+            echo "<li>$option</li>";
+        }
+        echo "</ul>";
+
+        echo "<p><strong>Answer:</strong> $answer</p>";
+    }
+}
+?>
